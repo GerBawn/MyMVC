6e1e05d6d39110654802678f2f48877ff23dea9f
GerBawn <lingchen410@163.com>
2016-04-09 18:59:07 +0800

    增加helper类文件

--- System/Core/Loader.php
+++ System/Core/Loader.php
@@ -10,8 +10,30 @@ namespace System\Core;
 
 class Loader
 {
+    /**
+     * 存储已经载入的文件
+     * @var array
+     */
+    private static $loaded = [];
+
     public static function autoload($class)
     {
         require BASEDIR . '/' . str_replace('\\', '/', $class) . '.php';
+    }
+
+    public function helper($name)
+    {
+        if (isset($loaded['helper'][$name])) {
+            return;
+        }
+        $fileName = SYSTEM_DIR . '/Helper/' . ucfirst($name) . '.php';
+        if (file_exists($fileName)) {
+            require $fileName;
+        }
+        $fileName = APP_DIR . '/Helper/' . ucfirst($name) . '.php';
+        if (file_exists($fileName)) {
+            require $fileName;
+        }
+        self::$loaded['helper'][$name] = $name;
     }
 }
---
+++ System/Helper/Helpers.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: GerBawn
+ * Date: 2016/4/9
+ * Time: 13:41
+ */
+
+function test()
+{
+    echo 'my name is lingchen.';
+}
