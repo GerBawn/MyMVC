ff7ee59921c033cf237d8763fb22d09236855ee2
GerBawn <lingchen410@163.com>
2016-06-06 09:59:42 +0800

    修改日志的显示方式，防止出现不必要的反斜线

--- System/Driver/Log/File.php
+++ System/Driver/Log/File.php
@@ -1,31 +1,28 @@
 <?php
-/**
- * User: GerBawn
- * Date: 2016/4/7 22:02
- */
 namespace System\Driver\Log;
 
 class File
 {
     private $config = [];
-    
+
     public function __construct($config)
     {
         $this->config = array_merge($this->config, $config);
     }
-    
+
     public function write($level, $message)
     {
         $time = date('Y-m-d H:i:s');
 
-        $msg = "{$level} | {$time} | {$message}".PHP_EOL;
+        $msg = "{$level} | {$time} | " . $message . PHP_EOL;
 
-        $logFile = $this->config['logPath'].'/'.$this->config['logFile'].'_'.date('Ymd').'.log';
+        $logFile = $this->config['logPath'] . '/' . $this->config['logFile'] . '_' . date('Ymd') . '.log';
 
         file_exists($this->config['logPath']) or mkdir($this->config['logPath'], 0755, true);
 
-        if(!$fp = fopen($logFile, 'a+'))
-            return ;
+        if (!$fp = fopen($logFile, 'a+')) {
+            return;
+        }
         flock($fp, LOCK_EX);
         fwrite($fp, $msg);
         flock($fp, LOCK_UN);
